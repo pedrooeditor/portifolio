b60ac088033ef70d70cb4397912ac7df14b57d2d
PedroHenrique <psimoes2704@gmail.com>
2026-09-16 03:48:27 -0300

    Conecta analytics ao Supabase

--- analytics-config.js
+++ analytics-config.js
@@ -1,8 +1,7 @@
 (() => {
-  // Preencha apenas com valores PUBLICOS do Supabase.
-  // Nunca coloque service_role, senha do banco ou qualquer chave secreta neste arquivo.
+  // Apenas valores PUBLICOS do Supabase. Nunca coloque service_role, senha do banco ou chaves secretas aqui.
   window.PH_ANALYTICS_CONFIG = Object.freeze({
-    supabaseUrl: '',
-    supabasePublishableKey: ''
+    supabaseUrl: 'https://pwbpxhsucidjgmlneksz.supabase.co',
+    supabasePublishableKey: 'sb_publishable_cO1ukxKyw8P9tdb2u8H92w_p-q3k-oy'
   });
 })();
