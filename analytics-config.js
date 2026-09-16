1ad33d72b6e9eb17422086dadad6c5d524c0253b
PedroHenrique <psimoes2704@gmail.com>
2026-09-16 03:29:42 -0300

    Adiciona configuracao base do analytics PH Motions

---
+++ analytics-config.js
@@ -0,0 +1,8 @@
+(() => {
+  // Preencha apenas com valores PUBLICOS do Supabase.
+  // Nunca coloque service_role, senha do banco ou qualquer chave secreta neste arquivo.
+  window.PH_ANALYTICS_CONFIG = Object.freeze({
+    supabaseUrl: '',
+    supabasePublishableKey: ''
+  });
+})();
